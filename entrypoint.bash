4c3d3ebb3dbbc2df5144c5223b50d212eb06d0ad
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2022-05-31 08:58:40 +0300

    msg

--- entrypoint.bash
+++ entrypoint.bash
@@ -2,24 +2,25 @@
 
 set -x
 
+PLATFORM_FLAVOR="linux-x64"
+TOP_DIR=${HOME}
 if [[ "${OSTYPE}" == "cygwin" || "${OSTYPE}" == "msys" ]]; then
   PLATFORM_FLAVOR="win32-x64"
   npm install --global yarn
-else
-  PLATFORM_FLAVOR="linux-x64"
+  TOP_DIR="/cygdrive/c"
 fi
 TARGET_DIR_NAME="VSCode-${PLATFORM_FLAVOR}"
 
-rm -rf ${HOME}/${TARGET_DIR_NAME}
-rm -rf ${HOME}/*.tar.gz
+rm -rf ${TOP_DIR}/${TARGET_DIR_NAME}
+rm -rf ${TOP_DIR}/*.tar.gz
 
 VSCODE="vscode"
-if [ ! -d "${HOME}/${VSCODE}/.git" ]; then
-   cd ${HOME}
+if [ ! -d "${TOP_DIR}/${VSCODE}/.git" ]; then
+   cd ${TOP_DIR}
    git clone https://github.com/microsoft/${VSCODE}.git
-   cd ${HOME}/${VSCODE}/
+   cd ${TOP_DIR}/${VSCODE}/
 else
-   cd ${HOME}/${VSCODE}/
+   cd ${TOP_DIR}/${VSCODE}/
    find "${PWD}" -type d -name 'node_modules' -exec rm -r {} \;
    git reset --hard
    git checkout main
@@ -38,5 +39,5 @@ else
 fi
 yarn gulp vscode-${PLATFORM_FLAVOR}-min
 
-cd ${HOME}
+cd ${TOP_DIR}
 tar -I 'gzip -9' -chf ./${TARGET_DIR_NAME}-${TOP_TAG}.tar.gz ./${TARGET_DIR_NAME}
